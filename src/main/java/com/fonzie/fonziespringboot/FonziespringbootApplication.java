6f25895299e892157aca77b1bec3041971a12f8f
arturbazza <agencia@fonzie.com.br>
2023-11-18 21:06:03 -0300

    Fonzie

---
+++ src/main/java/com/fonzie/fonziespringboot/FonziespringbootApplication.java
@@ -0,0 +1,13 @@
+package com.fonzie.fonziespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FonziespringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FonziespringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fonzie/fonziespringboot/FonziespringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fonzie.fonziespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FonziespringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
